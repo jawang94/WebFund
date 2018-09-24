8a82c065505bce3e025013daaa5a847eb552b709
Jason Wang <wang.jason.010@gmail.com>
2018-09-24 15:20:05 -0700

    algomodule2

---
+++ algorithms/algomodule2.js
@@ -0,0 +1,114 @@
+//Algorithms 2 Paper Algos I
+function multiply(x,y){
+    console.log(x);
+    console.log(y);
+}
+b = multiply(2,3);
+console.log(b);
+// logs 2
+// 3
+// undefined
+
+function multiply(x,y){
+    return x*y;
+}
+b = multiply(2,3);
+console.log(b);
+console.log(multiply(5,2));
+// logs 6
+// 10
+
+var x = [1,2,3,4,5,10];
+for(var i="keyword operator from-rainbow">=0; i<5; i++)
+{
+   i = i + 3; 
+   console.log(i);
+}
+// logs 3
+// 7
+
+
+var x=15;
+console.log(x);
+function awesome(){
+    var x=10;
+    console.log(x);
+}
+console.log(x);
+awesome();
+console.log(x);
+// logs 15
+// 15
+// 10
+// 15
+
+for(var i=0; i<15; i+=2){
+   console.log(i);
+}
+// logs 0
+// 2
+// 4
+// 6
+// 8
+// 10
+// 12
+// 14
+
+for(var i=0; i<3; i++){
+   for(var j=0; j<2; j++){       
+       console.log(i*j);
+   }
+}
+// logs 0
+// 0
+// 0
+// 1
+// 0
+// 2
+
+function looping(x,y){
+   for(var i=0; i<x; i++){
+      for(var j=0; j<x; j++){         
+          console.log(i*j);
+      } 
+   }
+}
+z = looping(3,3);
+console.log(z);
+// logs 0
+// 0
+// 0
+// 0
+// 1
+// 2
+// 0
+// 2
+// 4
+// undefined
+
+function looping(x,y){
+   for(var i=0; i<x; i++){
+      for(var j=0; j<y; j++){         
+         console.log(i*j);
+      } 
+   }
+   return x*y;
+}
+z = looping(3,5);
+console.log(z);
+// logs 0
+// 0
+// 0
+// 0
+// 0
+// 0
+// 1
+// 2
+// 3
+// 4
+// 0
+// 2
+// 4
+// 6
+// 8
+// 15
